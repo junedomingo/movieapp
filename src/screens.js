7138b1fcb66b7101e6f1b79d884e2902e7a86f34
June Domingo <june@cynder.io>
2016-10-17 14:02:38 +0800

    Register MoviesList component

--- src/screens.js
+++ src/screens.js
@@ -3,8 +3,10 @@ import { Navigation } from 'react-native-navigation';
 
 import SideMenu from './modules/_global/SideMenu';
 import Movies from './modules/movies/Movies';
+import MoviesList from './modules/movies/MoviesList';
 
 export function registerScreens(store, Provider) {
 	Navigation.registerComponent('movieapp.Movies', () => Movies, store, Provider);
+	Navigation.registerComponent('movieapp.MoviesList', () => MoviesList, store, Provider);
 	Navigation.registerComponent('movieapp.SideMenu', () => SideMenu);
 }
