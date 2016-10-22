4550be60236ca1ddecf34464b05381d4f419ade8
June Domingo <june@cynder.io>
2016-10-22 15:30:52 +0800

    Added Search screen

--- src/screens.js
+++ src/screens.js
@@ -5,10 +5,12 @@ import Drawer from './modules/_global/Drawer';
 import Movies from './modules/movies/Movies';
 import MoviesList from './modules/movies/MoviesList';
 import Movie from './modules/movies/Movie';
+import Search from './modules/movies/Search';
 
 export function registerScreens(store, Provider) {
 	Navigation.registerComponent('movieapp.Movie', () => Movie, store, Provider);
 	Navigation.registerComponent('movieapp.Movies', () => Movies, store, Provider);
 	Navigation.registerComponent('movieapp.MoviesList', () => MoviesList, store, Provider);
+	Navigation.registerComponent('movieapp.Search', () => Search, store, Provider);
 	Navigation.registerComponent('movieapp.Drawer', () => Drawer);
 }
