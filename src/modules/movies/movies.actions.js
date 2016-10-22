6f93d5a7aca2efdd1d1d41a80bc375a4856fdc7a
June Domingo <june@cynder.io>
2016-10-22 17:12:39 +0800

    Removed "retrieveTopRatedMovies" and "retrieveUpcomingMovies" actions, use "retrieveMoviesList" instead

--- src/modules/movies/movies.actions.js
+++ src/modules/movies/movies.actions.js
@@ -42,46 +42,6 @@ export function retrievePopularMovies(page) {
 	};
 }
 
-// TOP RATED
-export function retrieveTopRatedMoviesSuccess(res) {
-	return {
-		type: types.RETRIEVE_TOPRATED_MOVIES_SUCCESS,
-		topRatedMovies: res.data
-	};
-}
-
-export function retrieveTopRatedMovies(page) {
-	return function (dispatch) {
-		return axios.get(`${api.URL}/movie/top_rated?api_key=${api.KEY}&page=${page}`)
-		.then(res => {
-			dispatch(retrieveTopRatedMoviesSuccess(res));
-		})
-		.catch(error => {
-			console.log('Top Rated', error); //eslint-disable-line
-		});
-	};
-}
-
-// UPCOMING
-export function retrieveUpcomingMoviesSuccess(res) {
-	return {
-		type: types.RETRIEVE_UPCOMING_MOVIES_SUCCESS,
-		upcomingMovies: res.data
-	};
-}
-
-export function retrieveUpcomingMovies(page) {
-	return function (dispatch) {
-		return axios.get(`${api.URL}/movie/upcoming?api_key=${api.KEY}&page=${page}`)
-		.then(res => {
-			dispatch(retrieveUpcomingMoviesSuccess(res));
-		})
-		.catch(error => {
-			console.log('Upcoming', error); //eslint-disable-line
-		});
-	};
-}
-
 // NOW PLAYING
 export function retrieveNowPlayingMoviesSuccess(res) {
 	return {
