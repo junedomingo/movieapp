cca86ab5dc2bc8a1323dfa724a982ba85adefec1
June Domingo <june@cynder.io>
2016-11-13 00:16:30 +0800

    Get api config from .env

---
+++ src/constants/api.js
@@ -0,0 +1,8 @@
+import Config from 'react-native-config';
+
+export const TMDB_URL = Config.TMDB_URL;
+export const TMDB_IMG_URL = Config.TMDB_IMG_URL;
+export const TMDB_API_KEY = Config.TMDB_API_KEY;
+
+export const YOUTUBE_URL = Config.YOUTUBE_URL;
+export const YOUTUBE_API_KEY = Config.YOUTUBE_API_KEY;
