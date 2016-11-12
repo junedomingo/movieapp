829dbb0db5f7090247ae13ad93c11e86fc885621
June Domingo <june@cynder.io>
2016-11-13 00:01:44 +0800

    Read config from .env file

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,6 +1,8 @@
-export const URL = 'https://api.themoviedb.org/3';
-export const IMG_URL = 'https://image.tmdb.org/t/p';
-export const KEY = 'e50ddc772dd96f9aeb74b5236200b9d1';
+import Config from 'react-native-config';
 
-export const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/videos';
-export const YOUTUBE_KEY = 'AIzaSyB2fsaNSqkMmWlua7BISQ1Sc8Yh6XH-X5U';
+export const TMDB_URL = Config.TMDB_URL;
+export const TMDB_IMG_URL = Config.TMDB_IMG_URL;
+export const TMDB_API_KEY = Config.TMDB_API_KEY;
+
+export const YOUTUBE_URL = Config.YOUTUBE_URL;
+export const YOUTUBE_API_KEY = Config.YOUTUBE_API_KEY;
