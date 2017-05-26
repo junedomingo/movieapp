2692a7ceb9308ec71e30f5435c6ad667b43346e1
Andrew Fulton <afulton20@gmail.com>
2017-05-26 16:13:07 -0500

    Use cast_id instead of id.
    This seems to get rid of duplicate keys for the 'casts' tab of a Movie. Before, Guardians of the Galaxy Vol 2 would have duplicate keys.

--- src/modules/movies/tabs/Casts.js
+++ src/modules/movies/tabs/Casts.js
@@ -16,7 +16,7 @@ const Casts = ({ info, getTabHeight }) => {
 		<View style={styles.container} onLayout={getTabHeight.bind(this, 'casts', computedHeight)}>
 			{
 				info.casts.cast.map(item => (
-					<View key={item.id} style={styles.castContainer}>
+					<View key={item.cast_id} style={styles.castContainer}>
 						<Image source={{ uri: `${TMDB_IMG_URL}/w185/${item.profile_path}` }} style={styles.castImage} />
 						<View style={styles.characterContainer}>
 							<Text style={styles.characterName}>
