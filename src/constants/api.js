4349bd153f42d793ff581c704f114bb2a0e32887
Emre Tekince <emretekince@gmail.com>
2017-10-09 18:32:22 +0300

    Fix for network errors
    Some users cant read .env so it causes network errors. It is better to define configs directly at here.

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,8 +1,6 @@
-import Config from 'react-native-config';
+export const TMDB_URL = 'https://api.themoviedb.org/3';
+export const TMDB_IMG_URL = 'https://image.tmdb.org/t/p';
+export const TMDB_API_KEY = 'your_tmdb_api_key_here'
 
-export const TMDB_URL = Config.TMDB_URL;
-export const TMDB_IMG_URL = Config.TMDB_IMG_URL;
-export const TMDB_API_KEY = Config.TMDB_API_KEY;
-
-export const YOUTUBE_URL = Config.YOUTUBE_URL;
-export const YOUTUBE_API_KEY = Config.YOUTUBE_API_KEY;
+export const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/videos';
+export const YOUTUBE_API_KEY = 'your_youtube_api_key_here';
